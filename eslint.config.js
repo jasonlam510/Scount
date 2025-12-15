9649acfedc0e868c520a644bdc774430423ce9e5
jasonlam510 <79464531+jasonlam510@users.noreply.github.com>
2025-12-15 10:52:39 +0800

    chore: enhance ESLint configuration with global ignores and Prettier integration

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,20 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
+const { defineConfig, globalIgnores } = require('eslint/config');
 const expoConfig = require("eslint-config-expo/flat");
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = defineConfig([
+  globalIgnores([
+    "dist/*",
+    "node_modules/**",
+    "**/node_modules/@powersync/**"
+  ]),
   expoConfig,
+  eslintPluginPrettierRecommended,
   {
-    ignores: [
-      "dist/*",
-      "node_modules/**",
-      "**/node_modules/@powersync/**"
-    ]
+    files: ['babel.config.js', 'metro.config.js'],
+    languageOptions: {
+      globals: require('globals').node,
+    },
   }
 ]);

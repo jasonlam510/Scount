79f08a8e0f83278c0591435c394e8dee04ffcd31
jasonlam510 <79464531+jasonlam510@users.noreply.github.com>
2026-03-01 17:54:40 +0800

    chore: fix lint

--- src/powersync/index.ts
+++ src/powersync/index.ts
@@ -1,5 +1,10 @@
 // PowerSync exports - now using Kysely wrapper
-export { db, connectDatabase, disconnectDatabase } from "./SystemProvider.tsx";
+export {
+  db,
+  connectDatabase,
+  disconnectDatabase,
+  powerSync,
+} from "./SystemProvider";
 export { Connector } from "./Connector";
 export { AppSchema } from "./AppSchema";
 export type { Database } from "./AppSchema";

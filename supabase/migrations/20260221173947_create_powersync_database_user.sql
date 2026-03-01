11c8aa2e134865056520b422273f01e9c16a9b16
jasonlam510 <79464531+jasonlam510@users.noreply.github.com>
2026-03-01 17:40:41 +0800

    chore: add `if not exist` logic

--- supabase/migrations/20260221173947_create_powersync_database_user.sql
+++ supabase/migrations/20260221173947_create_powersync_database_user.sql
@@ -1,5 +1,11 @@
 -- Create a role/user with replication privileges for PowerSync
-CREATE ROLE powersync_role WITH REPLICATION BYPASSRLS LOGIN PASSWORD 'myhighlyrandompassword';
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'powersync_role') THEN
+    CREATE ROLE powersync_role WITH REPLICATION BYPASSRLS LOGIN PASSWORD 'myhighlyrandompassword';
+  END IF;
+END
+$$;
 -- Set up permissions for the newly created role
 -- Read-only (SELECT) access is required
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO powersync_role;  
